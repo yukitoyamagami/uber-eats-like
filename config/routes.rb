52e229199b216b8ca83aa9a4c7f9f2149c8d17fa
yukitoyamagami <yukiright1012@gmail.com>
2022-12-18 23:05:22 +0900

    write api routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create] do
+        member do
+          put :replace
+        end
+      end
+      resources :orders, only: %i[create]
+    end
+  end
 end
