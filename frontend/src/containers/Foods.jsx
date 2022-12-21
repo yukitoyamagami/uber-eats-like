3b01f74dcc8cdcf671d6eb149aff9282abf24d41
yukitoyamagami <yukiright1012@gmail.com>
2022-12-21 23:57:20 +0900

    setting components for show foods data detail

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,11 +1,48 @@
-import React, { Fragment, useEffect, useReducer } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import { LocalMallIcon } from '../components/Icons';
+import { FoodWrapper } from '../components/FoodWrapper';
+import { Skeleton } from '@mui/material';
 import {
   initialState as foodsInitialState,
   foodsActionTypes,
   foodsReducer,
 } from '../reducers/foods';
 import { fetchFoods } from '../apis/foods';
+import MainLogo from '../images/logo.png';
+import FoodImage from '../images/food-image.jpg';
+import { COLORS } from '../style_constants';
 import { REQUEST_STATE } from '../constants';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Foods = ({
   match
@@ -23,24 +60,44 @@ export const Foods = ({
           }
         });
       })
-  }, [])
+  }, []);
 
   return (
     <Fragment>
-      {
-        foodsState.fetchState === REQUEST_STATE.LOADING ?
-          <Fragment>
-            <p>
-              ロード中...
-            </p>
-          </Fragment>
-        :
-          foodsState.foodsList.map(food =>
-            <div key={food.id}>
-              {food.name}
-            </div>
-          )
-      }
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList>
+        {
+          foodsState.fetchState === REQUEST_STATE.LOADING ?
+            <Fragment>
+              {
+                [...Array(12).keys()].map(i =>
+                  <ItemWrapper key={i}>
+                    <Skeleton key={i} variant="rect" width={450} height={180} />
+                  </ItemWrapper>
+                )
+              }
+            </Fragment>
+          :
+            foodsState.foodsList.map(food =>
+              <ItemWrapper key={food.id}>
+                <FoodWrapper
+                  food={food}
+                  onClickFoodWrapper={(food) => console.log(food)}
+                  imageUrl={FoodImage}
+                />
+              </ItemWrapper>
+            )
+        }
+      </FoodsList>
     </Fragment>
   )
 }
