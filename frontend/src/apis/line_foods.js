ec8617d72ad672de35ece79e2095fd35a08558a0
yukitoyamagami <yukiright1012@gmail.com>
2022-12-23 00:22:01 +0900

    setting new api api call functions for replace line foods

--- frontend/src/apis/line_foods.js
+++ frontend/src/apis/line_foods.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
-import { lineFoods } from '../urls/index'
+import { lineFoods, lineFoodsReplace } from '../urls/index'
 
 export const postLineFoods =(params) => {
   return axios.post(lineFoods,
+    {
+      food_id: params.foodId,
+      count: params.count,
+    }
+  )
+  .then(res => {
+    return res.data
+  })
+  .catch((e) => { throw e; })
+};
+
+export const replaceLineFoods = (params) => {
+  return axios.put(lineFoodsReplace,
     {
       food_id: params.foodId,
       count: params.count,
